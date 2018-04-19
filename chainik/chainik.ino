cfb376903400aa64f2bfd819771c9d544e53e45f
Vilius <ponosak@gmail.com>
2018-04-19 18:25:30 +0300

    Temperaturos nustatymas, seka bluetooth statusa

--- chainik/chainik.ino
+++ chainik/chainik.ino
@@ -3,47 +3,65 @@
 #include <OneWire.h>
 
 #define ONE_WIRE_BUS 2
+
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
+
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
-int consoleRead;//reles testas is konsoles
-int temperature;
-int test,test2;
+int temperature; //Temperatura is temperaturos daviklio
+int input; //Kintamasis kuris saugo skaiciu iki 255(gauta is programeles)
+bool boiled; //loginis kintamasis skirtas patikrinti, ar vanduo uzvyre
+int state; //Bluetooth modulio prisijungimo statusas
+
 void setup(void)
 {
   Serial.begin(9600); //Begin serial communication
-  Serial.println("Arduino Digital Temperature // Serial Monitor Version"); //Print a message
   sensors.begin();
   pinMode(7,OUTPUT);//rele
-  pinMode(6,OUTPUT);//raudona
-  pinMode(3,OUTPUT);//melyna
+  pinMode(8,INPUT);//Bluetoth state
+  pinMode(6,OUTPUT);//raudonas LED
+  pinMode(3,OUTPUT);//melynas LED
 }
 
 void loop(void)
 { 
-  // Send the command to get temperatures
-  sensors.requestTemperatures();  
+  //Gauti temperatura is sensoriaus
+  sensors.requestTemperatures();
   temperature = (int)sensors.getTempCByIndex(0);
-  //test = (int)Serial.read();
-  //Serial.println(test);
-  //Serial.print("Temperature is: ");
-  if(test > 0)
-  {
-     test2 =test;
-     //Serial.println(test2);
+  input = Serial.read();
+  Serial.println(temperature);
+
+  //Apsauga --------------------------------------------------------------------------------------------------------------------------------------------
   
-  }
-  Serial.println(temperature); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  //Update value every 1 sec.
-  //a = Serial.read() - '0';
-  if(temperature >= 80)
-  {
+  state = digitalRead(8);
+  if (state == 0)
     digitalWrite(7,LOW);
-    
+
+  //Injungti arba isjungti ------------------------------------------------------------------------------------------------------------------------------
+  
+  if(input == 1)
+    digitalWrite(7,HIGH);
+  if(input == 0)
+    digitalWrite(7,LOW);
+
+  //Kaitinti iki nurodytos temperaturos (gaunamas skaicius nuo 100 iki 200 yra temperatura) -------------------------------------------------------------
+  
+  if (((temperature < 100) && ((input > 100) && (input < 200))))
+  {
+    digitalWrite(7,HIGH);
+    while (boiled)
+    {
+      if (temperature == 100)
+      {
+        boiled = false;
+        digitalWrite(7,LOW);
+      }
+    }
   }
-  else 
-  digitalWrite(7,HIGH);
+
+  //Keisti LED spalva -----------------------------------------------------------------------------------------------------------------------------------
+  
   analogWrite(6,((int)sensors.getTempCByIndex(0)*2.55));
   analogWrite(3,(255-(int)(sensors.getTempCByIndex(0)*2.55)));
   
